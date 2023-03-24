a26a95b13e625647e53a8b5dc1212789d7f541a3
FF <FF@FF>
2023-03-24 20:51:44 +0300

    Change comment of Database

--- BeanAndDrops/Program.cs
+++ BeanAndDrops/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//Connect to Database 
+//Connect to MySQL Community Database 
 builder.Services.AddDbContextPool<AppDbContext>(options =>
 	   options.UseMySQL(builder.Configuration.GetConnectionString("Defaults")));
 
